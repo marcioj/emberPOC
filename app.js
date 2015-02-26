ec223f39da94c4645c321a205d791e48d2471760
Waldir Borba Junior <wborbajr@gmail.com>
2015-02-26 00:55:13 -0300

    Initial Import

---
+++ app.js
@@ -0,0 +1,8 @@
+var App = Ember.Application.create({
+  LOG_ACTIVE_GENERATION: true,
+  LOG_MODULE_RESOLVER: true,
+  LOG_TRANSITIONS: true,
+  LOG_TRANSITIONS_INTERNAL: true,
+  LOG_VIEW_LOOKUPS: true,
+  DEBUG: true
+});
---
+++ router.js
@@ -0,0 +1,15 @@
+App.Router.map(function(){
+  this.route('teste',   {path:'/teste'});
+  // this.route('teste');
+
+});
+
+// var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
+
+// Router.map(function() {
+//   this.resource('teste',     {path:'/teste' });
+//   this.resource('photo.new',  {path:'/photo/new'});
+//   this.resource('photo',      {path:'/photo/:id'}, function(){
+//     this.route('edit');
+//   });
+// });
